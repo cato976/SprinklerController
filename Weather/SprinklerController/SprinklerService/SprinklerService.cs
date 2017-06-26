79c2e6d7e7cc6c83a328d4b05e9a505331b5c1c2
Andre Cato <ACato@quickquote.local>
2017-06-26 15:46:16 -0400

    Add Windows Service project

---
+++ Weather/SprinklerController/SprinklerService/SprinklerService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprinklerService
+{
+    public partial class SprinklerService : ServiceBase
+    {
+        private EventLog eventLog;
+
+        public SprinklerService()
+        {
+            InitializeComponent();
+            eventLog = new EventLog();
+            if(!EventLog.SourceExists("SprinklerSource"))
+            {
+                EventLog.CreateEventSource("SprinklerSource", "SprinklerLog");
+            }
+            eventLog.Source = "SprinklerSourve";
+            eventLog.Log = "SprinklerLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog.WriteEntry("Sprinkler Service Starting");
+        }
+
+        protected override void OnStop()
+        {
+            eventLog.WriteEntry("Sprinkler Service Stopping");
+        }
+    }
+}
